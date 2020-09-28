039867b8a41bac93fb351c9cbb49d8ab650ba0f3
enthusiast17 <ulan17nurym@gmail.com>
2020-09-28 23:31:11 +0500

    feat: add min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  return array.reduce((min, element) => Math.min(min, element), 0)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  return array.reduce((max, element) => Math.max(max, element), 0)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  return array.reduce((sum, element) => sum + element, 0) / array.length || 0
 }
